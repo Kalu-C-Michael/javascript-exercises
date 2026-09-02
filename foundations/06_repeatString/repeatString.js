9efebe946be63d70eaf5344c83b57e98d917c959
Kalu-C-Michael <kalumichael59@gmail.com>
2026-09-02 18:11:09 +0100

    repeatStrings solved

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = (string, num) => {
+ if (num < 0) {
+    return `ERROR`;
+ }
 
+ let reps = '';
+ for (let i = 0; i < num; i++) {
+  reps += string;
+ }
+ return reps;
 };
 
 // Do not edit below this line
